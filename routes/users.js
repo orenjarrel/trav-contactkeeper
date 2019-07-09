de764b95a00054524122076d2e65954343518262
orendavi <oren.davis@gmail.com>
2019-07-08 23:59:12 -0400

    Setup JWT token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -58,7 +59,24 @@ router.post('/', [
     // Save the new user
     await user.save();
 
-    res.send('User saved');
+    // Create an object payload
+    // this is the object we want to send in the JWT token
+    // with this ID, we can access all the contacts (specific data)
+    // ... that the user has
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    // to generate a token, we have to sign it
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 360000
+      // Callback: a funciton with a possible error; if no err, then return the token
+    }, (err, token) => {
+      if(err) throw err;
+      res.json({ token });
+    })
 
   } catch (err) {
     console.error(err.messeage);
