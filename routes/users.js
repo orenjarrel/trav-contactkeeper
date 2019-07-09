18a352ac58d6433639d11c732b3433c385b3b1f0
orendavi <oren.davis@gmail.com>
2019-07-08 23:49:27 -0400

    Added new user to mongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -17,18 +19,51 @@ router.post('/', [
     .isLength({
       min: 6
     })
-], (req, res) => {
+], 
+  async (req, res) => {
   // res.send(req.body);
   const errors = validationResult(req);
   // check if errors are empty
   if (!errors.isEmpty()) {
     return res.status(400)
-      // send json data; gives us an array of errors 
+      // send json data; return to us an array of errors in json format
       .json(
         { errors: errors.array() });
   }
 
-  res.send('passed');
+  // destructuring: the req.body should have "name, email, password"
+  // ... so we're destructuring to reduce code
+  const { name, email, password } = req.body
+
+  try {
+    // check to see if there is a user with this email
+    let user = await User.findOne({ email });
+
+    if(user) {
+      return res.status(400)
+        .json({ msg: 'User already exists '});
+    }
+
+    // create an instance of the User model
+    user = new User({
+      name,
+      email,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save the new user
+    await user.save();
+
+    res.send('User saved');
+
+  } catch (err) {
+    console.error(err.messeage);
+    res.status(500).send('Server Error')
+  }
 });
 
 
